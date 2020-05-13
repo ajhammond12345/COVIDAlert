f8efd7c7a3fe066633377eb941a1ed6defff66f0
Alexander Hammond <ajhammond123@gmail.com>
2020-05-13 12:41:30 -0400

    Initial Commit

---
+++ COVID Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID Alert
+//
+//  Created by Alexander Hammond on 5/13/20.
+//  Copyright © 2020 aajjr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
